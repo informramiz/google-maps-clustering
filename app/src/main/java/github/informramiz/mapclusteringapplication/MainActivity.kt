f5dcce3da7a2d17ee21dee63ee279887d875618d
Ramiz Raja <informramiz@gmail.com>
2019-08-10 17:20:55 +0500

    feat: Add map to activity

--- app/src/main/java/github/informramiz/mapclusteringapplication/MainActivity.kt
+++ app/src/main/java/github/informramiz/mapclusteringapplication/MainActivity.kt
@@ -1,19 +1,26 @@
 package github.informramiz.mapclusteringapplication
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mapFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        mapFragment.getMapAsync {
+            //TODO
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
