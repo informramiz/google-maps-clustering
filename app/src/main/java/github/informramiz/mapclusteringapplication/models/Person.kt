db1a99a824dcf7456782b7159b10c9049646e1d1
Ramiz Raja <informramiz@gmail.com>
2019-08-16 15:54:33 +0500

    refactor: Update model

--- app/src/main/java/github/informramiz/mapclusteringapplication/models/Person.kt
+++ app/src/main/java/github/informramiz/mapclusteringapplication/models/Person.kt
@@ -1,14 +1,18 @@
 package github.informramiz.mapclusteringapplication.models
 
+import androidx.annotation.DrawableRes
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.clustering.ClusterItem
+import github.informramiz.mapclusteringapplication.R
 
 
 /**
  * Created by Ramiz Raja on 2019-08-10.
  */
-class Person(private val name: String,
-             private val position: LatLng): ClusterItem {
+class Person(val location: LatLng,
+             val name: String,
+             @field:DrawableRes
+             val imageRes: Int = R.drawable.placeholder_image): ClusterItem {
     override fun getSnippet(): String? {
         return null
     }
@@ -18,6 +22,6 @@ class Person(private val name: String,
     }
 
     override fun getPosition(): LatLng {
-        return position
+        return location
     }
 }
