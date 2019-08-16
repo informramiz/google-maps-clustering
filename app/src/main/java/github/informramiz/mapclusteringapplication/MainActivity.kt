01736a1439fd1753eb8672415cd5cdc1f0ebf602
Ramiz Raja <informramiz@gmail.com>
2019-08-16 16:19:03 +0500

    feat: Use cluster manager in demo screen

--- app/src/main/java/github/informramiz/mapclusteringapplication/MainActivity.kt
+++ app/src/main/java/github/informramiz/mapclusteringapplication/MainActivity.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.clustering.ClusterManager
+import github.informramiz.mapclusteringapplication.models.Person
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var map: GoogleMap
+    private val random = Random(1984)
+    private val clusterManager by lazy {
+        ClusterManager<Person>(this, map)
+    }
     private val mapFragment by lazy {
         supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
     }
+    private val persons = mutableListOf<Person>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +31,10 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         mapFragment.getMapAsync {
-            //TODO
+            map = it
+            map.uiSettings.setAllGesturesEnabled(true)
+            map.uiSettings.isMapToolbarEnabled = true
+            startDemo()
         }
     }
 
@@ -37,5 +52,51 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun startDemo() {
+        clusterManager.renderer = PersonClusterRenderer(this, map, clusterManager)
+        initItems()
+        clusterManager.addItems(persons)
+        clusterManager.cluster()
+
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(persons[0].location, 8f))
+    }
+
+    private fun initItems() {
+        // http://www.flickr.com/photos/sdasmarchives/5036248203/
+        persons.add(Person(position(), "Walter", R.drawable.placeholder_image))
+
+        // http://www.flickr.com/photos/usnationalarchives/4726917149/
+        persons.add(Person(position(), "Gran", R.drawable.placeholder_image))
+
+        // http://www.flickr.com/photos/nypl/3111525394/
+        persons.add(Person(position(), "Ruth", R.drawable.placeholder_image))
+
+        // http://www.flickr.com/photos/smithsonian/2887433330/
+        persons.add(Person(position(), "Stefan", R.drawable.placeholder_image))
+
+        // http://www.flickr.com/photos/library_of_congress/2179915182/
+        persons.add(Person(position(), "Mechanic", R.drawable.placeholder_image))
+
+        // http://www.flickr.com/photos/nationalmediamuseum/7893552556/
+        persons.add(Person(position(), "Yeats", R.drawable.placeholder_image))
+
+        // http://www.flickr.com/photos/sdasmarchives/5036231225/
+        persons.add(Person(position(), "John", R.drawable.placeholder_image))
+
+        // http://www.flickr.com/photos/anmm_thecommons/7694202096/
+        persons.add(Person(position(), "Trevor the Turtle", R.drawable.placeholder_image))
+
+        // http://www.flickr.com/photos/usnationalarchives/4726892651/
+        persons.add(Person(position(), "Teach", R.drawable.placeholder_image))
+    }
+
+    private fun position(): LatLng {
+        return LatLng(random(51.6723432, 51.38494009999999), random(0.148271, -0.3514683))
+    }
+
+    private fun random(min: Double, max: Double): Double {
+        return random.nextDouble() * (max - min) + min
     }
 }
