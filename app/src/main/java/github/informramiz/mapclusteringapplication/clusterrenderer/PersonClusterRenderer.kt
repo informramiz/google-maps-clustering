4f093f3f215698bac41f4b124292c289d81dd2c1
Ramiz Raja <informramiz@gmail.com>
2019-08-16 18:44:44 +0500

    feat: Load image from web

--- app/src/main/java/github/informramiz/mapclusteringapplication/clusterrenderer/PersonClusterRenderer.kt
+++ app/src/main/java/github/informramiz/mapclusteringapplication/clusterrenderer/PersonClusterRenderer.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.maps.android.clustering.Cluster
 import com.google.maps.android.clustering.ClusterManager
@@ -25,7 +26,9 @@ class PersonClusterRenderer(
     private val markerIconGenerator = IconGenerator(context).apply {
         setStyle(IconGenerator.STYLE_PURPLE)
     }
+
     private val markerImageView: CircleImageView
+    private val picassoMarkers = mutableListOf<PicassoMarker>()
 
     init {
         //initialize the marker view
@@ -36,14 +39,24 @@ class PersonClusterRenderer(
     }
 
     override fun onBeforeClusterItemRendered(item: Person, markerOptions: MarkerOptions) {
-        markerImageView.setImageResource(item.imageRes)
         val icon = markerIconGenerator.makeIcon()
         markerOptions
             .icon(BitmapDescriptorFactory.fromBitmap(icon))
             .title(item.name)
     }
 
+    override fun onClusterItemRendered(clusterItem: Person, marker: Marker) {
+        val picassoMarker = PicassoMarker(marker, markerImageView, markerIconGenerator)
+        picassoMarker.load("https://cdn.pixabay.com/photo/2014/11/13/06/12/boy-529067_1280.jpg")
+        picassoMarkers.add(picassoMarker)
+    }
+
     override fun shouldRenderAsCluster(cluster: Cluster<Person>?): Boolean {
         return false
+    }
+
+    override fun onRemove() {
+        super.onRemove()
+        picassoMarkers.clear() 
     }
 }
