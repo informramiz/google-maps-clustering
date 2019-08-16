788e9f86c26a5560c733781ee271728fd0861630
Ramiz Raja <informramiz@gmail.com>
2019-08-16 19:06:58 +0500

    refactor: Use drawable to round image

--- app/src/main/java/github/informramiz/mapclusteringapplication/clusterrenderer/PicassoMarker.kt
+++ app/src/main/java/github/informramiz/mapclusteringapplication/clusterrenderer/PicassoMarker.kt
@@ -3,12 +3,14 @@ package github.informramiz.mapclusteringapplication.clusterrenderer
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.Marker
 import com.google.maps.android.ui.IconGenerator
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Target
 import github.informramiz.mapclusteringapplication.R
+import kotlin.math.min
 
 
 /**
@@ -27,10 +29,17 @@ class PicassoMarker (
         //ignore
     }
 
-    override fun onBitmapLoaded(bitmap: Bitmap?, from: Picasso.LoadedFrom) {
-        imageView.setImageBitmap(bitmap)
+    override fun onBitmapLoaded(bitmap: Bitmap, from: Picasso.LoadedFrom) {
+        imageView.setImageDrawable(bitmap.toCircularDrawable())
         val icon = iconGenerator.makeIcon()
         marker.setIcon(BitmapDescriptorFactory.fromBitmap(icon))
+    }
+
+    private fun Bitmap.toCircularDrawable(): Drawable {
+        val drawable = RoundedBitmapDrawableFactory.create(imageView.context.resources, this)
+        drawable.isCircular = true
+        drawable.cornerRadius = min(width.toFloat(), height / 2f)
+        return drawable
     }
 }
 
@@ -38,6 +47,5 @@ fun PicassoMarker.load(url: String) {
     Picasso.get().load(url)
         .resizeDimen(R.dimen.map_circle_image_view_size, R.dimen.map_circle_image_view_size)
         .placeholder(R.drawable.placeholder_image)
-        .transform(PicassoCircleTransform())
         .into(this)
 }
