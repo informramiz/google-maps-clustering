cad94f0421c15d47989add4249e9cbf31e8335e1
Ramiz Raja <informramiz@gmail.com>
2019-08-16 16:21:19 +0500

    refactor: Remove unnecessary code
    I am only interested in custom markers view and not custom clusters view

--- app/src/main/java/github/informramiz/mapclusteringapplication/PersonClusterRenderer.kt
+++ app/src/main/java/github/informramiz/mapclusteringapplication/PersonClusterRenderer.kt
@@ -2,7 +2,6 @@ package github.informramiz.mapclusteringapplication
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.widget.ImageView
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.MarkerOptions
@@ -25,11 +24,7 @@ class PersonClusterRenderer(
     private val markerIconGenerator = IconGenerator(context).apply {
         setStyle(IconGenerator.STYLE_PURPLE)
     }
-    private val clusterIconGenerator = IconGenerator(context)
-    private val dimension = context.resources.getDimension(R.dimen.map_circle_image_view_size)
-
     private val markerImageView: CircleImageView
-    private val clusterImageView: ImageView
 
     init {
         //initialize the marker view
@@ -37,10 +32,6 @@ class PersonClusterRenderer(
         markerIconGenerator.setContentView(markerView)
         markerIconGenerator.setBackground(null)
         markerImageView = markerView.findViewById(R.id.marker_image_view)
-
-        val clusterView = LayoutInflater.from(context).inflate(R.layout.custom_cluster_view, null)
-        clusterImageView = clusterView.findViewById(R.id.cluster_image_view)
-        clusterIconGenerator.setContentView(clusterView)
     }
 
     override fun onBeforeClusterItemRendered(item: Person, markerOptions: MarkerOptions) {
